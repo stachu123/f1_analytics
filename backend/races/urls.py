bdd7f5948534002c201a7e8b5fe2563f964e5aa6
stachu123 <166187@student.uwm.edu.pl>
2024-11-23 09:15:09 +0100

    starting to oad the data

--- backend/races/urls.py
+++ backend/races/urls.py
@@ -3,4 +3,5 @@ from .views import RaceListView
 
 urlpatterns = [
     path('races/<int:season>', RaceListView.as_view(), name='race-list'),
+    path('races/<int:id>/', RaceDetailView.as_view(), name='race-detail'),
 ]
--- backend/races/views.py
+++ backend/races/views.py
@@ -1,6 +1,11 @@
+import os
+import csv
+from django.http import JsonResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+
 from django.shortcuts import render
-
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Race
@@ -11,3 +16,20 @@ class RaceListView(APIView):
         races = Race.objects.filter(season=season)
         serializer = RaceSerializer(races, many=True)
         return Response(serializer.data)
+
+@csrf_exempt
+def get_race_data(request, race_id):
+
+    csv_directory = os.path.join(settings.MEDIA_ROOT, 'race_csvs')
+
+    file_path = os.path.join(csv_directory, f'{race_id}.csv')
+
+    if not os.path.exists(file_path):
+        raise Http404("File not found")
+
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        race_data = list(reader)
+
+    return JsonResponse(race_data, safe=False)
+
